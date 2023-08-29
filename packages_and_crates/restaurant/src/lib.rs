fb5418ca4db085ee3d85b45d96f739c77643155b
Onaconnapuna <cjgermain12@gmail.com>
2023-08-28 21:42:04 -0400

    upkeep

--- packages_and_crates/restaurant/src/lib.rs
+++ packages_and_crates/restaurant/src/lib.rs
@@ -1,14 +1,15 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        fn seat_at_table() {}
+    }
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    mod serving {
+        fn take_order() {}
+
+        fn serve_order() {}
+
+        fn take_payment() {}
     }
 }
