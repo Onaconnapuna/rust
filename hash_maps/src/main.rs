2c919f3d56517013aa6ed02154e939b83d3cc5f8
Onaconnapuna <cgermain12@gmail.com>
2023-08-29 16:30:29 -0400

    started hash maps 8.3

---
+++ hash_maps/src/main.rs
@@ -0,0 +1,22 @@
+fn hash_maps() {
+
+    // creating a hash map
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+}
+
+fn accessing_vals () {
+    use std::collections::HashMap;
+
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+}
