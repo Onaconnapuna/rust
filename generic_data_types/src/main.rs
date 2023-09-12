ca64c45cb9fe22676bb3b6c81a982f54d3793108
Onaconnapuna <cjgermain12@gmail.com>
2023-09-11 22:40:43 -0400

    generics cont

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
+// generics is a concept of reusing code that could take in different types of parameters to keep our code DRY
+
+fn premise() {
     let number_list = vec![34, 50, 25, 100, 65];
 
     let mut largest = &number_list[0];
@@ -10,4 +12,94 @@ fn main() {
     }
 
     println!("The largest number is {}", largest);
+}
+
+// if we want to do this again, we have to tediously write the code again; 
+// this is not ideal
+fn premise_again() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let mut largest = &number_list[0];
+
+    for number in &number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+}
+
+fn largest(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn instead() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+}
+
+fn largest_i32(list: &[i32]) -> &i32 {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+// how would we eliminate the duplication if the parameters were different type?
+
+
+fn largest_char(list: &[char]) -> &char {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest_i32(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
