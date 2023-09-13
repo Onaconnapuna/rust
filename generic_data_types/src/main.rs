3f47a4548e1bb146511e6eb904bc56fdfa0af222
Onaconnapuna <cjgermain12@gmail.com>
2023-09-12 22:29:32 -0400

    finished 10.1 Generic Data types

--- generic_data_types/src/main.rs
+++ generic_data_types/src/main.rs
@@ -65,6 +65,8 @@ fn instead() {
     let result = largest(&number_list);
     println!("The largest number is {}", result);
 }
+// how would we eliminate the duplication if the parameters were different type?
+// following 3 functions are the the same function, one with i32 and one with chars
 
 fn largest_i32(list: &[i32]) -> &i32 {
     let mut largest = &list[0];
@@ -77,7 +79,6 @@ fn largest_i32(list: &[i32]) -> &i32 {
 
     largest
 }
-// how would we eliminate the duplication if the parameters were different type?
 
 
 fn largest_char(list: &[char]) -> &char {
@@ -102,4 +103,171 @@ fn main() {
 
     let result = largest_char(&char_list);
     println!("The largest char is {}", result);
+}
+
+// Here is how we can make a more generic funciton
+// To define the generic largest function, place type name declarations inside angle brackets, <>, 
+// between the name of the function and the parameter list, like this:
+
+fn largest<T>(list: &[T]) -> &T { }
+
+
+// full generic function
+// this however wont compile, needs std::cmp::PartialOrd because using > we dont necessarily know the types
+
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
+
+//Generics in structs
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+}
+
+
+// wont compile, notice i32 type and float 
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+fn main() {
+    let wont_work = Point { x: 5, y: 4.0 };
+}
+
+// if we need to do this we could just have multilple generic type parameters
+struct Point<T, U> {
+    x: T,
+    y: U,
+}
+
+fn main() {
+    let both_integer = Point { x: 5, y: 10 };
+    let both_float = Point { x: 1.0, y: 4.0 };
+    let integer_and_float = Point { x: 5, y: 4.0 };
+}
+
+// In Enum definitions
+// revisiting option T, which is a generic
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+// revisiting Result
+enum Result<T, E> {
+    Ok(T),
+    Err(E),
+}
+
+//In fact, this is what we used to open a file in Listing 9-3, where T was filled in with the type std::fs::
+//File when the file was opened successfully and E was filled in with the type std::io::Error 
+//when there were problems opening the file.
+
+
+// Also method definitions
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+fn main() {
+    let p = Point { x: 5, y: 10 };
+
+    println!("p.x = {}", p.x());
+}
+
+// We can also specify constraints on generic types when defining methods on the type.
+// We could, for example, implement methods only on Point<f32> instances rather than on Point<T> instances with any generic type.
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+//This code means the type Point<f32> will have a distance_from_origin method;
+// other instances of Point<T> where T is not of type f32 will not have this method defined. 
+ 
+
+struct Point<X1, Y1> {
+    x: X1,
+    y: Y1,
+}
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c' };
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+}
+
+// no runtime cost
+
+//Rust accomplishes this by performing monomorphization of the code using generics at compile time.
+// Monomorphization is the process of turning generic code into specific code by filling in the concrete types 
+//that are used when compiled.
+
+
+let integer = Some(5);
+let float = Some(5.0);
+
+//Monomorphizatized 
+
+enum Option_i32 {
+    Some(i32),
+    None,
+}
+
+enum Option_f64 {
+    Some(f64),
+    None,
+}
+
+fn main() {
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
 }
