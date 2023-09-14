dca2f244306483e27721e88408ec26013d434a50
Onaconnapuna <cjgermain12@gmail.com>
2023-09-13 22:21:53 -0400

    upkeep

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,3 +1,92 @@
+//A trait defines functionality a particular type has and can share with other types. 
+//We can use traits to define shared behavior in an abstract way.
+
+//We want to make a media aggregator library crate named aggregator that can display
+// summaries of data that might be stored in a NewsArticle or Tweet instance.
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+} 
+
+//mplementing a trait on a type is similar to implementing regular methods. The difference is that after impl,
+//we put the trait name we want to implement, then use the for keyword, and then specify the name of the type 
+//we want to implement the trait for
+
+use aggregator::{Summary, Tweet};
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+}
+
+// 
+
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+use aggregator::{Summary, Tweet};
+
+let article = NewsArticle {
+    headline: String::from("Penguins win the Stanley Cup Championship!"),
+    location: String::from("Pittsburgh, PA, USA"),
+    author: String::from("Iceburgh"),
+    content: String::from(
+        "The Pittsburgh Penguins once again are the best \
+         hockey team in the NHL.",
+    ),
+};
+
+println!("New article available! {}", article.summarize());
+
+
+// 
+
+
+pub trait Summary {
+    fn summarize_author(&self) -> String;
+
+    fn summarize(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+impl Summary for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
 }
