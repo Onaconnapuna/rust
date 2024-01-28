59b78e86e2735c1cb3b9f0bf3e93e91c17e3b758
Onaconnapuna <cjgermain12@gmail.com>
2024-01-27 21:47:13 -0500

    finished quiz 1

--- rustlings/exercises/quiz1.rs
+++ rustlings/exercises/quiz1.rs
@@ -16,7 +16,13 @@
 // I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 { 
+   if apples > 40 {
+    apples
+   } else {
+    apples * 2
+   }
+}
 
 // Don't modify this function!
 #[test]
